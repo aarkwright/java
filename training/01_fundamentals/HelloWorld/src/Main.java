7e86c920921be2063a521b62fb911b0ed5ef5460
narkwright <adrian.psvmihaila@gmail.com>
2017-11-29 14:42:53 +0200

    Daily

---
+++ training/01_fundamentals/HelloWorld/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Simple comment
+    	System.out.println("Hello world 1");     // This is a comment
+        //System.out.println("Hello world 2");
+        System.out.println("Hello world 3");
+    }
+}
